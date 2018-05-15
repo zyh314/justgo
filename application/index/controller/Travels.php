12e07a2460b28f76408abf063debfc3ef999d4eb
zyh314 <578611109@qq.com>
2018-05-15 09:15:38 +0800

    0515

---
+++ application/index/controller/Travels.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\index\controller;
+use \think\Controller;
+use \think\Db;
+use \think\Config;
+use \think\Session;
+use \think\Redis;
+
+class Travels extends Controller
+{
+	protected $beforeActionList = [
+		'checkSession' => ['except' => 'del,user']
+	];
+	public function checkSession(){
+		return '进行验证';
+	}
+    public function index()
+    {
+        return $this->fetch('/travels');
+    }
+}
